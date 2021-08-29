7a325cacb5d346dffd66083e5e5eed46aed72f30
new_user <new_user@gmail.com>
2021-08-30 00:46:34 +0300

    Добавьте файлы проекта.

---
+++ CXANDFX/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Samples.HelloWorldTypeProvider;
+using System.Linq;
+using System.Diagnostics;
+
+namespace CXANDFX
+{
+    class MyClass
+    {
+        public static void Print(int  count)
+        {
+            foreach (var item in Enumerable.Range(0,count))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long C = 0 ;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            MyClass.Print(55);
+            stopwatch.Stop();
+            C = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+            stopwatch.Start();
+            MyModule.Print(55);
+            stopwatch.Stop();
+            Console.Clear();
+            Console.WriteLine(@"C#:{0}", C);
+            Console.WriteLine("F#:{0}",stopwatch.ElapsedMilliseconds);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassLibrary1/Library.fs
@@ -0,0 +1,6 @@
+﻿namespace Samples.HelloWorldTypeProvider
+
+module public MyModule=
+     let public Print x=for i in [0..x] do
+                        printfn "%d" i
+
